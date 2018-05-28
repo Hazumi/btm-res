175a893a7212693a19c0c6669d4a5646f2b4f6bd
Hazumi <shirohazumi@openmailbox.org>
2018-05-28 13:14:22 -0400

    added frequently visited page hotkeys

--- content.js
+++ content.js
@@ -12,5 +12,24 @@ document.onkeydown = function (event) {
     } else if (event.keyCode === 50 && event.altKey) {
         scriptsPos = scripts.offsetTop - 20;
         document.documentElement.scrollTop = scriptsPos;
+    } else if (event.keyCode === 97 && event.altKey) {
+        // Alt + NumPad 1 -> Query Tester
+        window.open('http://admin.bluetonemedia.com/admin/btmqs/db/', '_blank');
+    } else if (event.keyCode === 98 && event.altKey) {
+        // Alt + NumPad 2 -> Time Logger
+        window.open('http://admin.bluetonemedia.com/admin/btmtime/', '_blank');
+    } else if (event.keyCode === 99 && event.altKey) {
+        // Alt + NumPad 3 -> Time Report
+        window.open('http://admin.bluetonemedia.com/admin/btmtime/select.aspx', '_blank');
+    } else if (event.keyCode === 100 && event.altKey) {
+        // Alt + NumPad 4 -> Query Tester
+        window.open('http://admin.bluetonemedia.com/admin/btmqs/db/', '_blank');
+    } else if (event.keyCode === 100 && event.altKey) {
+        // Alt + NumPad 5 -> Training Docs
+        window.open('http://codes.bluetonemedia.com/docs-home', '_blank');
+    } else if (event.keyCode === 101 && event.altKey) {
+        // Alt + NumPad 6 -> Teamwork Hours
+        window.open('http://todo.mysiteserver.net/teamwork-dev-design', '_blank');
     }
+
 }
